d33ef33f2a67259fef3ccea9e8ec16c0bfd52621
Robert Thorpe II <rthorpeii@github.com>
2026-02-11 16:59:38 +0000

    Simplify header and add link to home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,39 +9,25 @@ import Image from "next/image";
 export default function Header() {
     return (
         <div className="flex flex-row relative bg-emerald-900 gap-5 justify-between items-center px-5">
-            <div>
-                <Image
-                    width={57}
-                    height={57}
-                    className="
-                    md:hidden
-                    rounded-full ...
+            <div className="absolute left-0 w-full flex flex-row items-center p-5 gap-5">
+                <a href="/" className="md:flex items-center gap-2">
+                    <Image
+                        width={57}
+                        height={57}
+                        className="
+                    rounded-full
                     bg-white
                     drop-shadow-lg
                     "
-                    src={logo.src}
-                    alt=""
-                />
+                        src={logo.src}
+                        alt=""
+                    />
+                    <p className="hidden md:block text-white text-2xl font-medium font-serif">
+                        Theatre@First
+                    </p>
+                </a>
             </div>
-            <div className="absolute left-0 w-full flex flex-row justify-center items-center p-5 gap-5">
-                <Image
-                    width={57}
-                    height={57}
-                    className="
-                    hidden
-                    md:flex
-                    rounded-full ...
-                    bg-white
-                    drop-shadow-lg
-                    "
-                    src={logo.src}
-                    alt=""
-                />
-                <p className="hidden md:flex text-white text-2xl font-medium font-serif">
-                    Theatre@First
-                </p>
-            </div>
-            <div className="flex flex-row p-5 items-center z-50">
+            <div className="flex flex-row p-5 items-center z-50 ml-auto">
                 <div className="flex flex-row p-5 gap-5 items-center">
                     <a href="/inventory"><Home width={24} height={24} className="text-white" /></a>
                     <a href="/item-upload"><Plus width={24} height={24} className="text-white" /></a>
